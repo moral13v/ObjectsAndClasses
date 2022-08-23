86d5af193a60b0ac9ba7f55f706e78f3d79812ec
Ivaylo Moraliev <ivaylo.moraliev@gmail.com>
2022-08-23 22:57:11 +0300

    Add project files.

---
+++ 03.Articles2.0/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Channels;
+using System.Globalization;
+
+namespace _03.Articles2._0
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Article> articles = new List<Article>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] articleData = Console.ReadLine().Split(", ");
+                Article newArticle = new Article(articleData[0], articleData[1], articleData[2]);
+                articles.Add(newArticle);
+            }
+
+            string orderType = Console.ReadLine();
+            switch (orderType)
+            {
+                case "title":
+                    articles = articles.OrderBy(art => art.Title).ToList();
+                    break;
+                case "content":
+                    articles = articles.OrderBy(art => art.Content).ToList();
+                    break;
+                case "author":
+                    articles = articles.OrderBy(art => art.Author).ToList();
+                    break;
+            }
+
+            foreach (var article in articles)
+            {
+                Console.WriteLine(article.ToString());
+            }
+        }
+    }
+
+    class Article
+    {
+        public Article(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+}
